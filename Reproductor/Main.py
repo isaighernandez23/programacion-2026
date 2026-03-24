89b260c144763301030189f6656ec10d1e62e12c
isaighernandez23 <isaighernandez23@ciencias.unam.mx>
2026-03-24 00:48:08 -0600

    Enhance Main.py with songs and playlist functionality
    Added multiple songs to the playlist and printed it.

--- Reproductor/Main.py
+++ Reproductor/Main.py
@@ -4,16 +4,16 @@ Created on March, 2026
 
 '''
 from Menu import *
+from Playlist import *
+
 class Main:
   pass
 
 cancion1 = Cancion("Hola", "Isai", 5, "Hip-Hop")
+cancion2= Cancion("i", "k",7,"Jazz")
+cancion3= Cancion("M","T",6,"Rap")
 
-menu = Menu("=== BIENVENIDO A TU BIBLIOTECA MUSICAL ===")
+playlist1=Playlist("Rolas",cancion1=cancion1,cancion2=cancion2,cancion3=cancion3)
 
-menu.dar_bienvenida()
-
-eleccion = menu.desplegar_menu()
-
-menu.procesar_opciones(eleccion, cancion1)
+print(playlist1)
 
