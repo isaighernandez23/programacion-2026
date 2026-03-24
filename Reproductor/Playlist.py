0910255ad231fa74e9b338fd72138e21d6d32a6d
isaighernandez23 <isaighernandez23@ciencias.unam.mx>
2026-03-24 00:46:34 -0600

    Add files via upload

---
+++ Reproductor/Playlist.py
@@ -0,0 +1,20 @@
+''' 
+Created on March, 2026
+@author: isaighernandez
+'''
+from Cancion import *
+class Playlist:
+    def __init__(self, nombre_playlist,cancion1, cancion2,cancion3):
+        self.nombre_playlist=nombre_playlist
+        self.cancion1=cancion1
+        self.cancion2=cancion2
+        self.cancion3=cancion3
+    
+    def __str__(self):
+        tmp = "Nombre de la playlist::" + str(self.nombre_playlist)
+        tmp += "\nCanción 1::" + str(self.cancion1)
+        tmp += "\nCanción 2::" + str(self.cancion2)
+        tmp += "\nCanción 3::" + str(self.cancion3)
+        return tmp
+    #def __str__(self):
+     #   return f"  {self.nombre_playlist} Lista de canciones:1.({self.cancion1}, 2.{self.cancion2},3{self.cancion3})"
