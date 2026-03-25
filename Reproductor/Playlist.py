49fd3b3a02df56c3d961b2c612740666ec23b9ff
isaighernandez23 <isaighernandez23@ciencias.unam.mx>
2026-03-25 00:14:45 -0600

    Update Playlist.py

--- Reproductor/Playlist.py
+++ Reproductor/Playlist.py
@@ -4,16 +4,19 @@ Created on March, 2026
 '''
 from Cancion import *
 class Playlist:
-    def __init__(self, nombre_playlist,cancion1, cancion2,cancion3):
+    def __init__(self, nombre_playlist,cancion1, cancion2):
         self.nombre_playlist=nombre_playlist
         self.cancion1=cancion1
         self.cancion2=cancion2
-        self.cancion3=cancion3
+        self.reproduciendo=False
+    
+    def reproducir(self):
+       self.reproduciendo = True
+       return f"REPRODUCIENDO:{self.nombre_playlist}"
     
     def __str__(self):
         tmp = "Nombre de la playlist::" + str(self.nombre_playlist)
         tmp += "\nCanción 1::" + str(self.cancion1)
         tmp += "\nCanción 2::" + str(self.cancion2)
-        tmp += "\nCanción 3::" + str(self.cancion3)
         return tmp
-   
+    
